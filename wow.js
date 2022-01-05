8b0cb1a95b4d75bedf4e61b02cb6c5fde34ccd4a
Mendovi <96690983+Mendovi@users.noreply.github.com>
2022-01-05 16:05:27 +0700

    Update wow.js

--- wow.js
+++ wow.js
@@ -23,6 +23,18 @@ document.getElementById("moto").style.left = xn+"px"
 }
 
 
+let socket = new WebSocket("wss://mendovi.github.io/");
+
+socket.onopen = function(e) {
+  alert("Отправляем данные на сервер");
+};
+
+function getCoordinates () {
+	if(xn != 0)
+	socket.send(xn);	
+}
+
+
 /*let socket = new WebSocket("wss://mendovi.github.io/");
 
 socket.onopen = function(e) {
@@ -31,48 +43,3 @@ socket.onopen = function(e) {
   socket.send(x);
 };
 
-function ifImageMove () {
-	if(dx != 0)
-		var jsonData
-		jsonData = JSON.stringify(imageMove);
-		console.log(jsonData);
-
-}
-
-/*var imageMove = {
-	dc = xn;
-}
-function ifImageMove () {
-	if(dx != 0)
-		var jsonData
-		jsonData = JSON.stringify(imageMove);
-		console.log(jsonData);
-
-}*/
-
-
-async function subscribe() {
-  let response = await fetch("/subscribe");
-
-  if (response.status == 502) {
-    // Статус 502 - это таймаут соединения;
-    // возможен, когда соединение ожидало слишком долго
-    // и сервер (или промежуточный прокси) закрыл его
-    // давайте восстановим связь
-    await subscribe();
-  } else if (response.status != 200) {
-    // Какая-то ошибка, покажем её
-    showMessage(response.statusText);
-    // Подключимся снова через секунду.
-    await new Promise(resolve => setTimeout(resolve, 1000));
-    await subscribe();
-  } else {
-    // Получим и покажем сообщение
-    let message = await response.text();
-    showMessage(message);
-    // И снова вызовем subscribe() для получения следующего сообщения
-    await subscribe();
-  }
-}
-
-subscribe();
